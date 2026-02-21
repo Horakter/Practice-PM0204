088a1e26fef48f47625ec5d0f8d816c4cee13620
Dmitriy <cppnullreference@gmail.com>
2026-02-21 19:18:33 +0300

    Add files via upload

---
+++ PM0204.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE EquipmentServiceDesk;
+GO
+USE EquipmentServiceDesk;
+
+insert into Equipment ([Name], InventoryNumber, [Location])
+values ('Рабочий компьютер №1', 'INV-001', 'Кабинет 101')
+
+
+INSERT INTO Users (Username, PasswordHash, Role)
+VALUES (
+'admin',
+'XohImNooBHFR0OVvjcYpJ3rj3V4w0uF9IYfYBf2o8YQ=',
+'Admin'
+);
